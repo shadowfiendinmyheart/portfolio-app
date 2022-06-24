0bbf25d07f5bea01ba0277742585d6915db01fb4
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2022-06-24 09:46:09 +0300

    [fix] select value

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useAppSelector } from "../../hooks/useRedux";
 
 import styles from "./Select.module.css";
 
@@ -8,12 +9,18 @@ interface SelectProps {
 }
 
 const Select: React.FC<SelectProps> = ({ options, onChange }) => {
+  const { currentMode } = useAppSelector((state) => state.cardSlice);
+
   const handleChangeValue = (event: React.ChangeEvent<HTMLSelectElement>) => {
     onChange(event.target.value);
   };
 
   return (
-    <select onChange={handleChangeValue} className={styles.select}>
+    <select
+      onChange={handleChangeValue}
+      value={currentMode}
+      className={styles.select}
+    >
       {options.map((option, index) => {
         return (
           <option value={option} key={option + index}>
